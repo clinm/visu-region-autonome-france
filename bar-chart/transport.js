785057f400170f9b77ae2f3763fee6f39a05163f
Matthieu Clin <matthieu.clin@outlook.com>
2017-01-31 17:38:56 +0100

    [Chart] Refactoring to object

--- bar-chart/transport.js
+++ bar-chart/transport.js
@@ -1,6 +1,6 @@
-var dataset;
+var CHART = {};
 
-var settings = {
+CHART.settings = {
     w: 600,
     h: 600,
     wAxeY: 100,
@@ -11,164 +11,192 @@ var settings = {
     duration: 500
 };
 
-var conf;
+CHART.chart = function(dataset, params) {
+
+    var xScale;
+
+    var yScale;
+
+    var xAxis;
+
+    var yAxis;
+
+    var data;
+
+    function handleBars() {
+        //Define key function
+        var key = function(d){return d.name};
+
+        var svg = d3.select(params.containerId).select("#chart");
+
+        var barPositionPipeline = function(selection) {
+            selection.attr("class", function(d){return d[params.displayValue] < 0 ? "negative" : "positive";})
+                .attr("x", function(d){
+                        return xScale(d.name);
+                    })
+
+                .attr("y", function(d){
+                        return yScale(Math.max(0, d[params.displayValue]));
+                    })
+                .attr("width", xScale.bandwidth())
+                .attr("height", function(d){
+                        return Math.abs(yScale(d[params.displayValue]) - yScale(0));
+                });
+        };
 
 
-function createChart(settings) {
-    //Create svg element
-    d3.select(settings.containerId).append("svg")
-        .attr("width", settings.w).attr("height", settings.h)
-        .attr("id", "chart");
-    //    .attr("viewBox", "0 0 "+w+ " "+h)
-    //    .attr("preserveAspectRatio", "xMinYMin");
+        svg.selectAll("rect")
+            .data(data, key)
+            .enter()
+            .append("rect")
+            .call(barPositionPipeline);
 
-    //Define tooltip for hover-over info windows
-    /*var div = d3.select("body").append("div")
-        .attr("class", "tooltip")
-        .style("opacity", 0);
-    */
-}
-
-//Define bar chart function
-function barChart(dataset, settings){
-    //Set width and height as fixed variables
-    var w = settings.w;
-    var h = settings.h;
-    var wAxeY = settings.wAxeY;
-    var hAxeX = settings.hAxeX;
-    var displayValue = settings.displayValue;
-
-    var data = dataset[settings.selectedYear]["regions"].sort(sortChoice(settings.sorted));
-
-    //
-    // returns an array of arrays where each element represents [min, max] for
-    // the given year
-    var extentEach = d3.extent(dataset, function(d) {
-        return d3.extent(d["regions"], function(a) {
-            return a[displayValue];
-        });
-    });
-
-    // merges arrays and retrieves global [min, max]
-    var yDomain = d3.extent(d3.merge(extentEach));
-
-    var yRangeBegin = 0;
-    if(yDomain[0] >= 0){
-        yRangeBegin = h - hAxeX;
-        yDomain[0] = 0;
-    }else {
-        yRangeBegin = h;
-    }
-    //Scale function for axes and radius
-    var yScale = d3.scalePow()
-        .domain(yDomain)
-        .range([yRangeBegin, 0]);
-
-    var xScale = d3.scaleBand()
-            .rangeRound([wAxeY, w])
-            .padding(0.1)
-            .domain(data.map(function(d){ return d.name;}));
-
-
-    //Create y axis
-    var yAxis = d3.axisLeft().scale(yScale);
-
-    var xAxis = d3.axisTop().scale(xScale);
-
-    //Define key function
-    var key = function(d){return d.name};
-
-    //Initialize state of chart according to drop down menu
-
-    var svg = d3.select(settings.containerId).select("#chart");
-    //Create bar chart
-
-    var barPositionPipeline = function(selection) {
-        selection.attr("class", function(d){return d[displayValue] < 0 ? "negative" : "positive";})
-            .attr("x", function(d){
-                    return xScale(d.name);
-                })
-
-            .attr("y", function(d){
-                    return yScale(Math.max(0, d[displayValue]));
-                })
-            .attr("width", xScale.bandwidth())
-            .attr("height", function(d){
-                    return Math.abs(yScale(d[displayValue]) - yScale(0));
-            });
-    };
-
-
-    svg.selectAll("rect")
-        .data(data, key)
-        .enter()
-        .append("rect")
-        .call(barPositionPipeline);
-
-    svg.selectAll("rect")
-        .data(data, key)
-        .transition()
-        .duration(settings.duration)
-        .call(barPositionPipeline);
-
-
-    var yAxisDom = svg.select(".y-axis");
-    if (yAxisDom.size()) {
-        yAxisDom.call(yAxis)
-    } else {
-        //Add y-axis
-        svg.append("g")
-            .attr("class", "y-axis")
-            .attr("transform", "translate("+ wAxeY + ",0)")
-            .call(yAxis);
-    }
-
-    var xAxisDom = svg.select(".x-axis");
-    var labels;
-    if (xAxisDom.size()) {
-        labels = xAxisDom
-            .attr("transform", "translate(0, "+ yScale(0) + ")")
+        svg.selectAll("rect")
+            .data(data, key)
             .transition()
-            .duration(settings.duration)
-            .call(xAxis)
-            .selectAll("text");
-    } else {
-        labels = svg.append("g")
-            .attr("class", "x-axis")
-            .attr("transform", "translate(0, "+ yScale(0) + ")")
-            .call(xAxis)
-            .selectAll("text")
-            .style("alignment-baseline", "middle")
-            .attr("transform", "rotate(-90)")
-            .attr("dy", 10);
+            .duration(params.duration)
+            .call(barPositionPipeline);
+
     }
 
+    function handleAxes() {
+        var svg = d3.select(params.containerId).select("#chart");
+        var yAxisDom = svg.select(".y-axis");
+        if (yAxisDom.size()) {
+            yAxisDom.call(yAxis)
+        } else {
+            //Add y-axis
+            svg.append("g")
+                .attr("class", "y-axis")
+                .attr("transform", "translate("+ params.wAxeY + ",0)")
+                .call(yAxis);
+        }
 
-    function isPositive(i) {
-        return data[i][displayValue]>= 0.0
+        var xAxisDom = svg.select(".x-axis");
+        var labels;
+        if (xAxisDom.size()) {
+            labels = xAxisDom
+                .attr("transform", "translate(0, "+ yScale(0) + ")")
+                .transition()
+                .duration(params.duration)
+                .call(xAxis)
+                .selectAll("text");
+        } else {
+            labels = svg.append("g")
+                .attr("class", "x-axis")
+                .attr("transform", "translate(0, "+ yScale(0) + ")")
+                .call(xAxis)
+                .selectAll("text")
+                .style("alignment-baseline", "middle")
+                .attr("transform", "rotate(-90)")
+                .attr("dy", 10);
+        }
+
+
+        function isPositive(i) {
+            return data[i][params.displayValue] >= 0.0;
+        }
+
+        labels.filter(function(d, i) { return isPositive(i)})
+            .style("text-anchor", "end")
+            .attr("dx", -10);
+
+
+        labels.filter(function(d, i) { return !isPositive(i); })
+            .style("text-anchor", "start")
+            .attr("dx", 10);
     }
 
-    labels.filter(function(d, i) { return isPositive(i)})
-        .style("text-anchor", "end")
-        .attr("dx", -10);
+    function updateAxisScale() {
+              //
+        // returns an array of arrays where each element represents [min, max] for
+        // the given year
+        var extentEach = d3.extent(dataset, function(d) {
+            return d3.extent(d["regions"], function(a) {
+                return a[params.displayValue];
+            });
+        });
+
+        // merges arrays and retrieves global [min, max]
+        var yDomain = d3.extent(d3.merge(extentEach));
+
+        var yRangeBegin = 0;
+        if(yDomain[0] >= 0){
+            yRangeBegin = params.h - params.hAxeX;
+            yDomain[0] = 0;
+        }else {
+            yRangeBegin = params.h;
+        }
+        //Scale function for axes and radius
+        yScale = d3.scalePow()
+            .domain(yDomain)
+            .range([yRangeBegin, 0]);
+
+        xScale = d3.scaleBand()
+                .rangeRound([params.wAxeY, params.w])
+                .padding(0.1)
+                .domain(data.map(function(d){ return d.name;}));
 
 
-    labels.filter(function(d, i) { return !isPositive(i); })
-        .style("text-anchor", "start")
-        .attr("dx", 10);
+        //Create y axis
+        yAxis = d3.axisLeft().scale(yScale);
 
-    //Function to sort data when sort box is checked
+        xAxis = d3.axisTop().scale(xScale);
+    }
+
     function sortChoice(sorted){
         if (sorted) {
-            return function(a,b){return b[displayValue] - a[displayValue];};
+            return function(a,b){return b[params.displayValue] - a[params.displayValue];};
         } else {
             return function(a,b){return d3.ascending(a.name, b.name);};
         }
     }
-}
+
+    var updateChart = function(settings) {
+        params = settings;
+
+        data = dataset[settings.selectedYear]["regions"].sort(sortChoice(settings.sorted));
+
+        updateAxisScale();
+
+        handleBars();
+
+        handleAxes();
+
+    };
+
+    var createChart = function() {
+        //Create svg element
+        d3.select(params.containerId).append("svg")
+            .attr("width", params.w).attr("height", params.h)
+            .attr("id", "chart");
+
+        return this;
+    };
+
+    var obj = {
+        update: updateChart,
+        create: createChart,
+        getConf: function() {
+            return params;
+        },
+        createSelect: function(selectID) {
+            d3.select(selectID)
+                .selectAll('option')
+                .data(dataset)
+                .enter()
+                .append('option')
+                .attr('value', function(d, i) {return i})
+                .text(function(d) {return d.year});
+        }
+    };
+
+    return obj.create();
+
+};
 
 
-
+var c;
 
 //Load data and call bar chart function
 d3.json("diff.json", function(error,data){
@@ -176,33 +204,29 @@ d3.json("diff.json", function(error,data){
     if(error){
         console.log(error);
     }else{
-        conf = $.extend({}, settings);
+        var conf = $.extend({}, CHART.settings);
         conf.displayValue = d3.select('input[name="choice"]:checked').attr('value');
 
-        createChart(conf);
-        barChart(data, conf);
+        c = CHART.chart(data, conf);
+        c.update(conf);
+        c.createSelect("#year");
     }
 
-    d3.select("#year")
-        .selectAll('option')
-        .data(data)
-        .enter()
-        .append('option')
-        .attr('value', function(d, i) {return i})
-        .text(function(d) {return d.year});
-
     $('input[name="choice"]').on('change', function() {
+        var conf = c.getConf();
         conf.displayValue = $(this).attr('value');
-        barChart(data, conf);
+        c.update(conf);
     });
 
     $('.checkbox').on('change', function() {
+        var conf = c.getConf();
         conf.sorted = $(this).is(':checked');
-        barChart(data, conf);
+        c.update(conf);
     });
 
     $('#year').on('change', function() {
+        var conf = c.getConf();
         conf.selectedYear = $(this).val();
-        barChart(data, conf);
+        c.update(conf);
     });
 });
