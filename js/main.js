2cc9f7add3b450f18cfd4789efe338d44a61dbee
ibtissam <ibtissam.elaousli@gmail.com>
2017-02-02 13:40:50 +0100

    Modification

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ jQuery(function($) {
 	
 
 	//smooth scroll
-	$('.navbar-nav > li').click(function(event) {
-		event.preventDefault();
+	$('.navbar-nav > li.anchor').click(function(event) {
+		//event.preventDefault();
 		var target = $(this).find('>a').prop('hash');
 		$('html, body').animate({
 			scrollTop: $(target).offset().top
